9b08f778132be86d95211c861e7c8a88327d0a6d
Rafael Wendel Pinheiro <rafaelwendel@hotmail.com>
2016-05-31 15:49:05 -0300

    Metodos de insertBatch no Usuario_model

--- application/models/Usuario_model.php
+++ application/models/Usuario_model.php
@@ -35,4 +35,50 @@ class Usuario_model extends CI_Model {
                 ->get()
                 ->row_object();
     }
+    
+    public function insertBatch($list)
+    {
+        $verifica = $this->verificarDuplicidade($list, array('usuario'));
+        if(is_array($verifica['validos']) && count($verifica['validos']) > 0){
+            $this->db->insert_batch('usuario', $verifica['validos']);
+        }        
+        return $verifica;
+    }
+    
+    public function verificarDuplicidade($list, $fields)
+    {
+        $field = array();
+        $duplicados = array();
+        
+        foreach ($fields as $f){
+            $field[$f] = $this->getArrayOf($f);
+        }
+        
+        $x = 0;
+        foreach ($list as $data){
+            foreach ($data as $key => $value){
+                if(in_array($key, $fields)){
+                    if(in_array($value, $field[$key])){
+                        $duplicados[] = $list[$x];
+                        unset($list[$x]);
+                    }
+                }            
+            }
+            $x++;
+        }
+        
+        return array('validos' => $list, 'duplicados' => $duplicados);
+    }
+    
+    protected function getArrayOf($param)
+    {
+        $result = $this->db->select($param)->from('usuario u')->get()->result_array();
+        $array = array();
+        
+        foreach ($result as $res){
+            $array[] = $res[$param];
+        }
+        
+        return $array;
+    }
 }
